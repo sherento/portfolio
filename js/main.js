a7c8a66c9f2219977de78e31f76919b81a1a9fc9
sheren <sheren.t@hotmail.com>
2018-02-06 13:15:57 +1100

    added changes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
 // console.log(jQuery, $);
 
-// $(document).ready(function() {
-//   $(".enter").click(function() {
-//     $(".homeContainer").hide();
-//     $(".main").show();
-//     $("nav").show();
-//   });
-// });
-
 function myFunction(x) {
   x.classList.toggle("change");
   $("nav").toggle();
